3787cd6f4253108548b7c52823a6ecb93e48a41c
Michiel Gerritsen <michiel@controlaltdelete.nl>
2024-04-18 20:56:05 +0200

    Base urls + assets

--- .devcontainer/start-container.sh
+++ .devcontainer/start-container.sh
@@ -2,4 +2,6 @@ composer install && \
 cp .env.example .env && \
 php artisan key:generate && \
 touch database/database.sqlite && \
-php artisan migrate
+php artisan migrate && \
+npm ci && \
+npm run build
